78194d57263feab02eef0a3b089a774c70d409d6
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 15:10:07 +0530

    Create environment.h

---
+++ assignment-3/part3_dynamic_path/environment.h
@@ -0,0 +1,11 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#define ROW 10
+#define COL 10
+
+void initializeGrid(int grid[ROW][COL]);
+void printGrid(int grid[ROW][COL]);
+void updateObstacles(int grid[ROW][COL]);
+
+#endif
