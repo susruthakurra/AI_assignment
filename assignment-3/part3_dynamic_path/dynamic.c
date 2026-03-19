43e4596d7a64c73330f649ddd8385225398e0d2a
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 15:13:45 +0530

    Update dynamic.c

--- assignment-3/part3_dynamic_path/dynamic.c
+++ assignment-3/part3_dynamic_path/dynamic.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "environment.h"
 
 int main() {
-    printf("Dynamic obstacle simulation\n");
+    int grid[ROW][COL];
+
+    srand(time(0));
+
+    initializeGrid(grid);
+
+    printf("Initial Grid:\n");
+    printGrid(grid);
+
+    for(int step = 1; step <= 5; step++) {
+        addDynamicObstacle(grid);
+
+        printf("\nAfter step %d (new obstacle added):\n", step);
+        printGrid(grid);
+    }
+
     return 0;
 }
