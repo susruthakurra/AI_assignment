98ce95f51170e71c9710a191d69e4716f2272035
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 14:24:36 +0530

    Update dijkstra.c

--- assignment-3/part1_dijkstra/dijkstra.c
+++ assignment-3/part1_dijkstra/dijkstra.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "utils.h"
 
 int main() {
-    printf("Dijkstra Part\n");
+    int graph[MAX][MAX] = {
+        {0, 10, 0, 30, 100},
+        {10, 0, 50, 0, 0},
+        {0, 50, 0, 20, 10},
+        {30, 0, 20, 0, 60},
+        {100, 0, 10, 60, 0}
+    };
+
+    int n = 5;
+    int start = 0;
+
+    dijkstra(graph, n, start);
+
     return 0;
 }
