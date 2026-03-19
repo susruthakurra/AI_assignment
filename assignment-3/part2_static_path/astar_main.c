6b382f56088f4426c7d58f5612ee09ff563cf209
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 14:45:43 +0530

    Update astar_main.c

--- assignment-3/part2_static_path/astar_main.c
+++ assignment-3/part2_static_path/astar_main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "grid.h"
+
+// Function declaration
+void addObstacles(int grid[ROW][COL], int density);
 
 int main() {
-    printf("A* Pathfinding\n");
+    int grid[ROW][COL];
+
+    initializeGrid(grid);
+    addObstacles(grid, 15);
+
+    printf("Grid with obstacles:\n");
+    printGrid(grid);
+
     return 0;
 }
