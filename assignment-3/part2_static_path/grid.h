e905fdd5c511065782e912f08bb4f5b26cd18256
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 15:00:27 +0530

    Update grid.h

--- assignment-3/part2_static_path/grid.h
+++ assignment-3/part2_static_path/grid.h
@@ -4,6 +4,15 @@
 #define ROW 10
 #define COL 10
 
+typedef struct {
+    int x, y;
+} Point;
+
+typedef struct {
+    int parent_x, parent_y;
+    int f, g, h;
+} Cell;
+
 void initializeGrid(int grid[ROW][COL]);
 void printGrid(int grid[ROW][COL]);
 
