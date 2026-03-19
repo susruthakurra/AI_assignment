abbe1351279e6519c69a9149f9a7796d68b923b4
susruthakurra <kurrasusrutha@gmail.com>
2026-03-19 14:26:42 +0530

    Update dijkstra.c

--- assignment-3/part1_dijkstra/dijkstra.c
+++ assignment-3/part1_dijkstra/dijkstra.c
@@ -2,18 +2,33 @@
 #include "utils.h"
 
 int main() {
-    int graph[MAX][MAX] = {
-        {0, 10, 0, 30, 100},
-        {10, 0, 50, 0, 0},
-        {0, 50, 0, 20, 10},
-        {30, 0, 20, 0, 60},
-        {100, 0, 10, 60, 0}
-    };
+    int graph[MAX][MAX], n;
+    char cities[MAX][50];
 
-    int n = 5;
-    int start = 0;
+    FILE *file = fopen("graph_data.txt", "r");
 
-    dijkstra(graph, n, start);
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    fscanf(file, "%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        fscanf(file, "%s", cities[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            fscanf(file, "%d", &graph[i][j]);
+        }
+    }
+
+    fclose(file);
+
+    int start = 0; // Hyderabad
+
+    dijkstra(graph, n, start, cities);
 
     return 0;
 }
