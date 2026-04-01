7455a493905c6f74b5f21525208992e7b44f326e
susruthakurra <kurrasusrutha@gmail.com>
2026-04-01 19:10:41 +0530

    Create sudoku.py

---
+++ assignment-4/sudoku-csp/sudoku.py
@@ -0,0 +1,48 @@
+grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+def is_valid(row, col, num):
+    for i in range(9):
+        if grid[row][i] == num or grid[i][col] == num:
+            return False
+
+    start_row = row - row % 3
+    start_col = col - col % 3
+
+    for i in range(3):
+        for j in range(3):
+            if grid[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def solve():
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(row, col, num):
+                        grid[row][col] = num
+                        if solve():
+                            return True
+                        grid[row][col] = 0
+                return False
+    return True
+
+def print_grid():
+    for row in grid:
+        print(row)
+
+solve()
+
+print("Solved Sudoku:")
+print_grid()
